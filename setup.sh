fefec54937848242ab45471d72691283b72c2cfa
uesteibar <uesteibar@gmail.com>
2018-01-06 22:24:39 +0100

    Add sonarr server

--- setup.sh
+++ setup.sh
@@ -2,13 +2,15 @@ echo "-> Creating folders"
 
 mkdir ~/mediacenter
 mkdir ~/mediacenter/plex_config
+mkdir ~/mediacenter/sonarr_config
 mkdir ~/mediacenter/downloads
+mkdir ~/mediacenter/downloads/series
 
 echo "-> Creating nginx configuration"
 
 source .env
 
-PLEX_DOMAIN=$PLEX_DOMAIN TRANSMISSION_DOMAIN=$TRANSMISSION_DOMAIN \
+PLEX_DOMAIN=$PLEX_DOMAIN TRANSMISSION_DOMAIN=$TRANSMISSION_DOMAIN SONARR_DOMAIN=$SONARR_DOMAIN \
   erb ./nginx/nginx.conf.erb > ./nginx/nginx.conf
 
 echo "\nDone!"
